e3606e629f8cc804baecb9c4cd278f4bcd6d48cc
karina-kovaleva <karina_kovaleva.03@mail.ru>
2022-10-31 08:37:05 +0300

    5.additionTableView

--- HomeWork9_Karina (race)/ViewControllers/RecordsViewController.swift
+++ HomeWork9_Karina (race)/ViewControllers/RecordsViewController.swift
@@ -6,48 +6,66 @@
 //
 
 import UIKit
+         
+protocol RecordsViewControllerDelegate {
+    func changeColor(color: UIColor)
+}
 
-class RecordsViewController: UIViewController {
-
-    lazy var labelForResults: UILabel = {
-        var labelForResults = UILabel()
-        labelForResults.numberOfLines = 0
-        labelForResults.textAlignment = .center
-        labelForResults.lineBreakMode = .byWordWrapping
-        labelForResults.text = "Racer - Score\n"
-        labelForResults.font = UIFont(name: "Marker Felt", size: 30)
-        labelForResults.frame = CGRect(x: self.view.frame.origin.x, y: self.view.frame.size.height / 10, width: self.view.frame.size.width, height: self.view.frame.size.height * 0.7)
-        return labelForResults
+class RecordsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, RecordsViewControllerDelegate {
+    
+    var tableViewForRecords: UITableView = {
+        var tableViewForRecords = UITableView()
+        tableViewForRecords.translatesAutoresizingMaskIntoConstraints = false
+        tableViewForRecords.layer.cornerRadius = 30
+        return tableViewForRecords
     }()
     
-    func updateRecords() {
-        guard var resultsArray = UserDefaults.standard.value([Results].self, forKey: "records") else { return }
-        var text = "Racer - Score\n"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.8363788724, green: 0.9316909909, blue: 0.7094728351, alpha: 1)
+        self.view.addSubview(tableViewForRecords)
+        
+        tableViewForRecords.dataSource = self
+        tableViewForRecords.delegate = self
+        
+        tableViewForRecords.register(RecordsTableViewCell.self, forCellReuseIdentifier: "RecordsTableViewCell")
+        
+        tableViewForRecords.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 110 ).isActive = true
+        tableViewForRecords.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10).isActive = true
+        tableViewForRecords.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10).isActive = true
+        tableViewForRecords.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -110).isActive = true
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 17
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var arrayOfNames = [String]()
+        var arrayOfScores = [String]()
+        var cell: UITableViewCell!
+        let tempCell = tableView.dequeueReusableCell(withIdentifier: RecordsTableViewCell.id, for: indexPath) as! RecordsTableViewCell
+        guard var resultsArray = UserDefaults.standard.value([Results].self, forKey: "records") else { return cell }
         while resultsArray.count > 17 {
             resultsArray.removeFirst()
         }
         for results in resultsArray {
-            if results.name == "" {
-                text += ("Unnamed Racer - \(String(describing: results.score))\n")
-                labelForResults.text = text
-            } else {
-                
-                text += ("\(String(describing: results.name)) - \(String(describing: results.score))\n")
-                labelForResults.text = text
-            }
+            arrayOfNames.append(results.name)
+            arrayOfScores.append(results.score)
         }
-        text = ""
+        arrayOfNames = arrayOfNames.map { $0 == "" ? "Unnamed Racer" : $0 }
+        tempCell.labelForName.text = ("\(String(describing: arrayOfNames[indexPath.row])) - \(String(describing: arrayOfScores[indexPath.row]))")
+        cell = tempCell
+        tempCell.delegate = self
+        return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.8363788724, green: 0.9316909909, blue: 0.7094728351, alpha: 1)
-        self.view.addSubview(labelForResults)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        updateRecords()
-        
+    func changeColor(color: UIColor) {
+        self.view.backgroundColor = color
     }
 }
